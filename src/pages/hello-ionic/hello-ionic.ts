d4e56337bef4e4a7416aee6b71c81ca96671db12
Bruno Willian <bwdesouza@gmail.com>
2018-11-08 15:14:03 -0200

    Ajustes na tela inicial e entrando na te de detalhes.

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { ItemDetailsPage } from '../item-details/item-details';
+import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
 
+  item : any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  }
+
+  chamaQuiz(animal, caminhoFoto){
+    this.item = {
+      titulo: animal,
+      caminhoFoto: caminhoFoto
+    }
+    
+    this.navCtrl.push(ItemDetailsPage, {
+      item: this.item
+    });
   }
 }
